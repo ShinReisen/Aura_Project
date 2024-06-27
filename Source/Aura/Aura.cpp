58f1243594842c1596580356b4857963f3375e2a
ShinReisen <shinzeroone@gmail.com>
2024-06-27 20:46:28 +0200

    The base character class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Shindorai
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Shindorai
+
+#pragma once
+
+#include "CoreMinimal.h"
+
